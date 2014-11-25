912c99db85b78f84195ecc1e7cd1af75a77148b4
Mark Stillwell <mark@stillwell.me>
2014-11-25 12:26:28 +0000

    install in home directory by default...

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ VERSION = 0.5
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
+PREFIX = ${HOME}/Programs/pkgs/st-git
 MANPREFIX = ${PREFIX}/share/man
 
 X11INC = /usr/X11R6/include
